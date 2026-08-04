7ffdb49d407c1581d614e933e3c4be014878295f
SebastianSPM <sebasperemald04@gmail.com>
2026-08-04 12:54:12 -0500

    Agregando estructura del proyecto, agregando funcionalidad, añadiendo estilos - Archivos: index.html, style.css, app.js

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# APOD
+
+Es un programa que publica diariamente una imagen o video astronómico acompañado de una breve explicación científica Estas imágenes incluyen fotos de galaxias, nebulosas, planetas y eventos cósmicos, capturadas por telescopios como el Hubble.
+
+## Estructura del proyecto
+
+## Tecnologias usadas
+
+HTML
+CSS
+JavaScript
+
+## Pasos para usar
+
+1. git clone o descargar zip
+2. abrir en visual studio code o editor favorito
+3. usar un servidor o extensión
+4. abrir proyecto
+
+## Participantes
+
+- Sebastian Pereira
+- David Quiroz
+- Juan Camilo Acevedo
+- Deyvid Santiago Moreno
---
+++ script/app.js
@@ -0,0 +1,79 @@
+const API = "https://api.nasa.gov/planetary/apod";
+
+const API_KEY = "d4QAPpeEEQcVUGygvgGPAgGJchF5kylNMI9vpiIR"
+
+const cargando = () => {
+    Swal.fire({
+        title: "Cargando...",
+        Text: "Estamos cargando los datos",
+        allowOutsideClick: false,
+        didOpen: () => {
+            Swal.showLoading();
+        }
+    });
+}
+
+cargando();
+
+fetch(`${API}?api_key=${API_KEY}`)
+.then(response => {
+    validarAPI(response.ok);
+
+    if(!response.ok){
+        throw new Error("Hay un error en la apis");
+    }
+    return response.json();
+})
+.then(data => {
+    Swal.close();
+    renderizar(data);
+})
+.catch(error => {
+    console.error("Hay un error: ", error);
+})
+
+const validarAPI = (ok) => {
+    if(ok){
+        console.log("TODO OK");
+    }else{
+        console.log("FALLO ALGO");
+    }
+}
+
+const renderizar = (data) => {
+    const contenedor = document.getElementById("contenedor");
+    contenedor.innerHTML = `
+        <section>
+            <h2>Titulo: <span>${data.title}</span></h2>
+            <img class="favoritosImagen" src="../assets/images/heart-fill.svg" alt="" />
+            <h3>Fecha: ${data.date}</h3>
+            <img class="imagenApi" src="${data.url}" alt="${data.title}">
+            <p class="descripcionApi">${data.explanation}</p>
+            <input id="cambiarFecha" type="date" value="${data.date}">
+        </section>
+    `
+    document.getElementById("cambiarFecha").addEventListener("change", (event) => {
+        const fecha = event.target.value;
+        nuevaImagen(fecha);
+    });
+}
+
+const nuevaImagen = (fecha = "") => {
+    let urlNormal = `${API}?api_key=${API_KEY}`;
+
+    if(fecha){
+        urlNormal += `&date=${fecha}`;
+    }
+
+    fetch(urlNormal)
+    .then(response => response.json())
+    .then(data => {
+        renderizar(data);
+    })
+}
+
+const favoritos = JSON.parse(localStorage.getItem("favoritos")) || [];
+
+favoritos.push(data)
+
+localStorage.setItem("favoritos", JSON.stringify(favoritos))
